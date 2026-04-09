c58e105660fdc14df323bb58412b2e1aa3c08eaa
Aliano <portifolio@aliano.com.br>
2026-04-09 12:12:57 -0300

    Ajustando a base para a raiz da Vercel

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,6 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
-  base: '/jesus-na-web-react/', // ⚠️ MUDE para o nome do seu repositório
+  base: '/', 
   plugins: [react()],
 })
