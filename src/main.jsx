73154f2bdf4771e7ec7746b5f99829a4ee88cc59
Aliano <portifolio@aliano.com.br>
2026-05-26 18:59:27 -0300

    "feat: adiciona o vercel analytics para contagem de visitas"

--- src/main.jsx
+++ src/main.jsx
@@ -3,6 +3,12 @@ import ReactDOM from 'react-dom/client'
 import { HashRouter } from 'react-router-dom'
 import App from './App.jsx'
 
+// 1. Importa o contador da Vercel
+import { inject } from '@vercel/analytics';
+
+// 2. Ativa o contador automaticamente no site
+inject();
+
 ReactDOM.createRoot(document.getElementById('root')).render(
   <HashRouter>
     <App />
